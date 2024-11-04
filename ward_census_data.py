4d8051bf3b35a1c13ce79ae970c461a3bee268c1
Jack Russell <jack132@gmail.com>
2024-11-04 12:37:23 +0000

    Adds the deltas map

--- ward_census_data.py
+++ ward_census_data.py
@@ -4,6 +4,7 @@ import pydeck as pdk
 import streamlit as st
 
 GEOJSON_PATH = 'ward_data_strings.geojson'
+DELTAS_PATH = 'ward_delta_strings.geojson'
 
 st.set_page_config(page_title='Census data by ward 2021',
                    page_icon='https://storage.mlcdn.com/account_image/293832/H3etpcTDIeoZhChu7cYp9RkbGecqwFM8aYZLhbWb.png')
@@ -15,6 +16,11 @@ st.header('Northern Ireland Census 2021: Religious mix by ward')
 @st.cache_data
 def load_ward_data():
     data = gpd.read_file(GEOJSON_PATH)
+    return data
+
+@st.cache_data
+def load_ward_deltas():
+    data = gpd.read_file(DELTAS_PATH)
     return data
 
 # Explain the source of the data and link to it
@@ -32,6 +38,49 @@ age_band = st.selectbox(
 rel_layer = pdk.Layer(
     'GeoJsonLayer',
     gdf[gdf['AgeBand'] == age_band],
+    opacity=0.8,
+    stroked=True,
+    filled=True,
+    extruded=False,
+    get_fill_color=('[red, green, blue]'),
+    get_line_color=[200, 200, 200, 150],
+    line_width_min_pixels=1,
+    pickable=True,
+)
+
+tooltip1 = {
+    'html': '{tooltip}',
+    'style': {
+        'backgroundColor': 'steelblue',
+        'color': 'white',
+        'border': '1px solid white',
+        'fontSize': '12px',
+        'padding': '5px',
+    }
+}
+
+view_state = pdk.ViewState(latitude=54.7, longitude=-6.7, zoom=7, bearing=0, pitch=0)
+
+r = pdk.Deck(layers=[rel_layer], initial_view_state=view_state, tooltip=tooltip1)
+
+# st.subheader('Religious mix per DEA')
+# st.markdown('Hover over to access aggregate pupil percentages by self-declared religion.')
+
+# render the map
+st.pydeck_chart(r)
+
+
+# Explain the source of the data and link to it
+st.markdown("This map visualises the change in religious categories between the 2011 and 2021 censuses. "
+            "This data is for all age bands. Wards are coloured by the increase in the percentage of "
+            "the population identifying as having no religion."
+            )
+
+ddf = load_ward_deltas()
+
+rel_layer = pdk.Layer(
+    'GeoJsonLayer',
+    ddf,
     opacity=0.8,
     stroked=True,
     filled=True,
